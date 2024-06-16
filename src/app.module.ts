8f65c9d1e70e334e3d5af5d92cf25c49ab931a7d
ateals <ateals@icloud.com>
2024-06-16 16:20:34 +0900

    feat: type safe ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { TypeSafeConfigModule } from './common/config/config.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TypeSafeConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
