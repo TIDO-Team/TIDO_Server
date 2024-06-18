7de99ae6cb1de19987c778361e058412e48fd7b5
ateals <ateals@icloud.com>
2024-06-18 18:09:40 +0900

    feat(#1): UserModule, AuthModule 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { TypeSafeConfigModule } from './common/config/config.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, TypeSafeConfigModule],
+  imports: [PrismaModule, TypeSafeConfigModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
