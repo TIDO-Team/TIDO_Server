5d5af9c8bd089fa0436e6a667908b54487b388e9
ateals <ateals@icloud.com>
2024-06-18 20:14:46 +0900

    fix(#1): 토큰가드 global guard로 설정 + @IsPublic 데코레이터

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { PrismaModule } from './common/prisma/prisma.module';
 import { TypeSafeConfigModule } from './common/config/config.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AccessTokenGuard } from './auth/guard/bearer-token.guard';
 
 @Module({
   imports: [PrismaModule, TypeSafeConfigModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,14 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/users.dto';
+import { IsPublic } from '@/common/decorator/isPublic.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @IsPublic()
   async findAll() {
     return this.usersService.getAll();
   }
